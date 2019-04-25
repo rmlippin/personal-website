38fefc1492ef89f7d1c5ff0e385b3668c37c7f70
Raquel Lipp <rmlippin@gmail.com>
2019-04-24 20:59:38 -0700

    Fixed carousel sizings

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+   var height = $(window).height() - 210;  //getting windows height
+   console.log("Height: ", height);
+   $('.carousel .item').css('height',height+'px');
+   $('.carousel-inner').css('height',height+'px');   //and setting height of carousel
+});
+
+$(window).resize(function(){
+   var height = $(window).height() - 210;  //getting windows height
+   console.log("Height: ", height);
+   $('.carousel-item').css('height',height+'px');
+   $('.carousel-inner').css('height',height+'px');   //and setting height of carousel
+});
+
 $(function(){
 	$('.header').load("header.html"); 
 	$('.footer').load("footer.html");
